21180814836b0872632d0d5f57fa5ef5a218212b
Andrei Paulau <andrei.paulau@regula.by>
2020-08-20 16:25:57 +0300

    Update docker_import.sh

--- face-api/docker_import.sh
+++ face-api/docker_import.sh
@@ -14,8 +14,8 @@ if [ $# -ge 2 ]; then
     echo $0: usage: $0 REPO_NAME ECR_URL
     exit 1
 fi
-###Uncomment and remove ") at $AWS_REGION)" to use AWS credentials profile with permission to AWS ECR
-eval $(aws ecr get-login-password --region $AWS_REGION) ## --profile $AWS_PROFILE)
+### add --profile at "aws ecr" command to use AWS credentials profile with permission to AWS ECR
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_URL
 docker build -t "$REPO_NAME" .
 docker tag "$REPO_NAME":latest "$ECR_URL"/"$REPO_NAME":latest
 docker push "$ECR_URL"/"$REPO_NAME":latest
